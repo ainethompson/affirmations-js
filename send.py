2ad0d6c19c01a0e515b02bb1a221c0ce8dc88ef5
Áine Thompson <ainegthompson@gmail.com>
2020-11-13 04:36:19 +0000

    Working on getting user by phone number

--- send.py
+++ send.py
@@ -12,43 +12,62 @@ if __name__== '__main__':
     from server import app
     model.connect_to_db(app)
 
-# TO SEND TO EVERY USER:
-all_phones = crud.get_all_phone_nums()
-phone_list = []
-
-for phone_num in all_phones:
-    phone_str =''.join(list(phone_num)) #  ('510-981-9837',) --> 510-981-9837
-    raw_phone = phone_str.replace('-', '') #  510-981-9837 --> 5109819837
-    
-    phone_list.append(f'+1{raw_phone}') #  5109819837 --> +15109819837
-
-unsent_messages = crud.get_unsent_messages()
-
 def send_message():
-    client = Client(twilio_sid, auth_token)
-
-    i = randint(0, len(unsent_messages))
-    to_send = unsent_messages[i]
-
-    text = to_send.text
-    author = to_send.author
-
-    quote = f"✨{text} \n\n- {author} ✨"
-
     twilio_number = '+15103300507'
     # phone ='+15109819837'
+    
+# TO SEND TO EVERY USER:
+    all_phones = crud.get_all_phone_nums()
+    phone_list = []
+
+    for phone_num in all_phones:
+        phone_str =''.join(list(phone_num)) #  ('510-981-9837',) --> 510-981-9837
+        raw_phone = phone_str.replace('-', '') #  510-981-9837 --> 5109819837
+        
+        phone_list.append(f'+1{raw_phone}') #  5109819837 --> +15109819837
+
     for num in phone_list:
         phone = num
-    
-    message = client.messages.create(to=phone,
-                            from_=twilio_number,
-                            body=quote)
-    print(message)
+
+        client = Client(twilio_sid, auth_token)
+
+        unsent_messages = crud.get_unsent_messages()
+        i = randint(0, len(unsent_messages))
+        to_send = unsent_messages[i]
+
+        text = to_send.text
+        author = to_send.author
+        quote = f"✨{text} \n\n- {author} ✨"
+        
+        message = client.messages.create(to=phone,
+                                from_=twilio_number,
+                                body=quote)
+        print(message)
 
     # to do: format phone from '+10000000000' to '000-000-0000'
+
+    loop throu each phone num to get list of each char
+    return f string use indeces to get order
+
+   
+    for phone in phone_list:
+        chars = []
+        for i in range(len(phone)):
+            chars.append(phone[i])
+        print (chars)
+        chars.pop(0)
+        print (chars)
+
+
+
+
+# phone_list = ['+15109819837', '+15109147993', '+15108470677']
+
+
+        # db_phone = phone.strip
     # user = crud.get_user_by_phone(phone_str)
 
-    # crud.create_user_message(user, to_send)
+    crud.create_user_message(user, to_send)
     
     crud.update_to_sent(to_send)
 
