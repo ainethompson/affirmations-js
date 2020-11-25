6cf26fdd150828a83e2d2af45ce8d368a1c86cb0
Áine Thompson <ainegthompson@gmail.com>
2020-11-25 00:37:21 +0000

    Got randomMessage to run using fetch

--- static/js/randomMessage.jsx
+++ static/js/randomMessage.jsx
@@ -7,33 +7,49 @@ function RandomMessage() {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        $.post('/api/message-generator', (res) => {
-            const randMsg = $('#randMsg');
-            randMsg.html(`<p>${res.text}</p>
-            <p>- ${res.author}</p>`);
-            console.log(res.text, res.author);
-        });
+        //     $.post('/api/message-generator', (res) => {
+        //         const randMsg = $('#randMsg');
+        //         randMsg.html(`<p>${res.text}</p>
+        //         <p>- ${res.author}</p>`);
+        //         console.log(res.text, res.author);
+        //     });
+        // }
+
+        fetch('/api/message-generator', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        })
+            .then(response => response.json())
+            .then(function (response) {
+                const randMsg = document.getElementById('randMsg');
+                randMsg.innerHTML = `<p>${response.text}</p>
+                    <p>- ${response.author}</p>`;
+            });
+
     }
 
 
-        // const randMsg = document.querySelector('#randMsg');
+    // const randMsg = document.querySelector('#randMsg');
 
-        // fetch('/api/message-generator', {
-        //     method: 'POST'
-        // }) 
-        //     .then((res) => {
-        //         // const randMsg = document.querySelector('#randMsg');
-        //         randMsg.html(`<p>${res.text}</p>
-            
-        //     // .then((mes) => {
+    // fetch('/api/message-generator', {
+    //     method: 'POST'
+    // }) 
+    //     .then((res) => {
+    //         // const randMsg = document.querySelector('#randMsg');
+    //         randMsg.html(`<p>${res.text}</p>
 
-        //     // })
-        //         .html(res.text);
-                    
-        //             // `<p>${res.text}</p>
-        //             // <p>- ${res.author}</p>`);
-        //         console.log(res.text, res.author);
-        //     });
+    //     // .then((mes) => {
+
+    //     // })
+    //         .html(res.text);
+
+    //             // `<p>${res.text}</p>
+    //             // <p>- ${res.author}</p>`);
+    //         console.log(res.text, res.author);
+    //     });
 
     return (
         <div>
