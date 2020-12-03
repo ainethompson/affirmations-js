96f54e196d96317bb1549b2553de6d8862590b11
Áine Thompson <ainegthompson@gmail.com>
2020-12-03 05:44:55 +0000

    Added more messages to data/

--- send.py
+++ send.py
@@ -1,33 +1,34 @@
 from twilio.rest import Client
 import json
-import model
+from model import connect_to_db
 import crud
 import schedule
 import time
 from random import choice, randint
 
-if __name__== '__main__':
+if __name__ == '__main__':
     from server import app
-    model.connect_to_db(app)
+    connect_to_db(app)
 
-    secrets_dict = json.loads(open('data/secrets.json').read())
-    twilio_sid = secrets_dict["TWILIO_ACCOUNT_SID"]
-    auth_token = secrets_dict["TWILIO_AUTH_TOKEN"]
-    message_service_sid = secrets_dict["MESSAGING_SERVICE_SID"]
+secrets_dict = json.loads(open('data/secrets.json').read())
+twilio_sid = secrets_dict["TWILIO_ACCOUNT_SID"]
+auth_token = secrets_dict["TWILIO_AUTH_TOKEN"]
+message_service_sid = secrets_dict["MESSAGING_SERVICE_SID"]
 
+# twilio_number = '+15103300507'
+client = Client(twilio_sid, auth_token)
 
-    # twilio_number = '+15103300507'
-    client = Client(twilio_sid, auth_token)
-    
 # TO SEND TO EVERY USER:
-    all_phones = crud.get_all_confirmed_phones()
-    phone_list = []
+all_phones = crud.get_all_confirmed_phones()
+phone_list = []
 
-    for phone_num in all_phones:
-        phone_str =''.join(list(phone_num)) #  ('510-981-9837',) --> 510-981-9837
-        raw_phone = phone_str.replace('-', '') #  510-981-9837 --> 5109819837
-        
-        phone_list.append(f'+1{raw_phone}') #  5109819837 --> +15109819837
+for phone_num in all_phones:
+    # ('510-981-9837',) --> 510-981-9837
+    phone_str = ''.join(list(phone_num))
+    raw_phone = phone_str.replace('-', '')  # 510-981-9837 --> 5109819837
+
+    phone_list.append(f'+1{raw_phone}')  # 5109819837 --> +15109819837
+
 
 def send_message():
 
@@ -46,7 +47,7 @@ def send_message():
         to_remove = "[],' "
         new_str = sections
         for item in to_remove:
-            new_str = new_str.replace(item,'')
+            new_str = new_str.replace(item, '')
         print(new_str)
 
         user = crud.get_user_by_phone(new_str)
@@ -54,12 +55,12 @@ def send_message():
         text = to_send.text
         author = to_send.author
         quote = f"✨ Good morning {user.name} ✨ \n\n{text} \n\n- {author}"
-        
+
         message = client.messages.create(to=phone,
-                                messaging_service_sid=message_service_sid,
-                                body=quote)
+                                         messaging_service_sid=message_service_sid,
+                                         body=quote)
         print(message)
-        
+
         crud.create_user_message(user, to_send)
 
     crud.update_to_sent(to_send)
@@ -77,5 +78,5 @@ schedule.every(10).seconds.do(send_message)
 # schedule.every().day.at("19:39").do(send_message)
 
 while True:
-        schedule.run_pending()
-        time.sleep(1)
+    schedule.run_pending()
+    time.sleep(1)
